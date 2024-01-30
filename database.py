d421c760eb09b6183514686e1159be1193f275bb
Bird <vc77bird@gmail.com>
2024-01-30 08:39:11 -0500

    Update database settings

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sys import platform
 
 URL_DATABASE = 'sqlite:///../database/eReg.db'
+
+if platform == "linux" or platform == "linux2":
+    URL_DATABASE = "sqlite:////code/database/eReg.db"
 
 engine = create_engine(URL_DATABASE, connect_args={"check_same_thread": False})
 
